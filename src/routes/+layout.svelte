59d2e8225ded1b91544e94da56e19c06d6406865
sergiobarria <sbarria.dev@gmail.com>
2024-03-23 18:36:03 -0500

    initial setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '@fontsource-variable/montserrat';
+	import '@fontsource/lato/300.css';
+	import '@fontsource/lato/400.css';
+	import '@fontsource/lato/700.css';
+
+	import '../app.css';
+</script>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+	Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam ipsum, totam sit doloribus enim eos
+	a quae? Sequi facere commodi incidunt dolorum, debitis suscipit exercitationem omnis adipisci
+	consectetur itaque sint? Nemo ullam aspernatur aut vitae minus, quo quidem praesentium quam
+	consequuntur amet natus voluptate nostrum ab eos accusamus ea voluptatibus iure, repellendus harum
+	odit eligendi at dolorum id impedit? Nobis? Expedita quas, saepe corrupti debitis fugit voluptatem
+	modi deserunt ipsam aspernatur maiores omnis ipsa voluptates, assumenda, vel impedit corporis.
+	Excepturi, ipsam quae? Vel atque, in consequatur aliquam sed doloribus tenetur. Magni non expedita
+	sit dolor, eius vel aut corrupti perferendis voluptatum illo? Autem distinctio asperiores quod
+	enim et modi totam dolorem, vel, ipsum, fugiat aperiam voluptatem nulla doloribus sunt dolores!
+	Corporis officiis quos incidunt? Laudantium minus nam, alias voluptate repellat, facere magni
+	tempora aliquid, quos ea veniam enim laboriosam quibusdam eaque molestias adipisci eligendi? Id
+	alias quos beatae distinctio cumque.
+</p>
